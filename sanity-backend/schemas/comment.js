8b330df48fb46e30f772bb97012083cce95de380
Bhavesh <bhaveshjat9950@gmail.com>
2022-07-04 13:01:24 +0530

    initial commit

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+	name: "comment",
+	title: "Comment",
+	type: "document",
+	fields: [
+		{
+			name: "postedBy",
+			title: "Posted by",
+			type: "string",
+		},
+		{
+			name: "comment",
+			title: "Comment",
+			type: "string",
+		},
+	],
+};
